92afdb3f1bf8b8ac36979e5d889b512491100d71
Julian Storer <julianstorer@gmail.com>
2025-05-19 09:56:18 +0100

    Refactored InputDevice and OutputDevice to have a common base class

--- modules/tracktion_engine/playback/devices/tracktion_WaveInputDevice.cpp
+++ modules/tracktion_engine/playback/devices/tracktion_WaveInputDevice.cpp
@@ -1365,9 +1365,8 @@ protected:
 };
 
 //==============================================================================
-WaveInputDevice::WaveInputDevice (Engine& e, const juce::String& devType,
-                                  const WaveDeviceDescription& desc, DeviceType t)
-    : InputDevice (e, devType, desc.name, "wavein_" + juce::String::toHexString (desc.name.hashCode())),
+WaveInputDevice::WaveInputDevice (Engine& e, const WaveDeviceDescription& desc, DeviceType t)
+    : InputDevice (e, desc.name, "wavein_" + juce::String::toHexString (desc.name.hashCode())),
       deviceChannels (desc.channels),
       deviceType (t),
       channelSet (createChannelSet (desc.channels))
@@ -1380,6 +1379,14 @@ WaveInputDevice::~WaveInputDevice()
 {
     notifyListenersOfDeletion();
     closeDevice();
+}
+
+juce::String WaveInputDevice::getDeviceTypeDescription() const
+{
+    if (deviceType == trackWaveDevice)
+        return TRANS("Track Wave Input");
+
+    return TRANS("Wave Audio Input");
 }
 
 juce::StringArray WaveInputDevice::getMergeModes()
@@ -1514,7 +1521,7 @@ void WaveInputDevice::saveProps()
 juce::String WaveInputDevice::getSelectableDescription()
 {
     if (getDeviceType() == trackWaveDevice)
-        return getAlias() + " (" + getType() + ")";
+        return getAlias() + " (" + getDeviceTypeDescription() + ")";
 
     return InputDevice::getSelectableDescription();
 }
